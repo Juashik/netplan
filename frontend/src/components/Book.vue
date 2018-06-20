a5ed5ada07e79487f73b13190a62d1b8ffbfd609
Juashik <juashik@gmail.com>
2018-06-21 01:29:40 +0300

    Temp

--- frontend/src/components/Book.vue
+++ frontend/src/components/Book.vue
@@ -1,9 +1,9 @@
 <template>
 <div id='component'>
   <div class="step">
-    <h3>Шаг {{ steps[0].id }} из 15</h3>
+    <h3>Шаг {{ id }} из 15</h3>
     <img src="../assets/7.png" alt="">
-    <p>{{ steps[0].text }}</p>
+    <p>{{ text }}</p>
   </div>
   <div v-if='false' id="loading">
     <img id="loading_img" src="../assets/loading.gif" alt=""/>
@@ -14,15 +14,40 @@
 <script>
 export default {
   name: 'Book',
-  data () {
-    return {
-      steps: [
-        { id: '7', text: 'Критические работы – b3, b10, b11. Резервы времени этих работ равны нулю. Выделим критический путь двойными стрелками' },
-        { id: '2', text: 'Lorem ipsum 2' }
-      ]
+  data: {
+    steps: [
+      { id: "1", text: "Lorem ipsum 1" },
+      { id: "2", text: "Lorem ipsum 2" },
+      { id: "3", text: "Lorem ipsum 3" },
+      { id: "4", text: "Lorem ipsum 4" },
+      { id: "4", text: "Lorem ipsum 4" },
+      { id: "4", text: "Lorem ipsum 4" },
+      {
+        id: "7",
+        text:
+          "Критические работы – b3, b10, b11. \
+          Резервы времени этих работ равны нулю. \
+          Выделим критический путь двойными стрелками"
+      },
+      { id: "8", text: "Lorem ipsum 8" },
+      { id: "9", text: "Lorem ipsum 9" },
+      { id: "10", text: "Lorem ipsum 10" },
+      { id: "11", text: "Lorem ipsum 11" },
+      { id: "12", text: "Lorem ipsum 12" },
+      { id: "13", text: "Lorem ipsum 13" },
+      { id: "14", text: "Lorem ipsum 14" },
+      { id: "15", text: "Lorem ipsum 15" }
+    ]
+  },
+  computed: {
+    id () {
+      return this.$route.params.id
+    },
+    text () {
+      return this.steps[this.$route.params.id].text
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -14,7 +14,7 @@ export default new Router({
       component: Main
     },
     {
-      path: '/book',
+      path: '/book/:id',
       component: Book
     },
     {
