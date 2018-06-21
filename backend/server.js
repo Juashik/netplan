b1a8506d5322f0dfb5037ef7438aa6a3df604fd6
Juashik <juashik@gmail.com>
2018-06-21 09:19:40 +0300

    Api port 80

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const config = require('./config/main');
 const app = express();
 
-const port = 3000;
+const port = 80;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
